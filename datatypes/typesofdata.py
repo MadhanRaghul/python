2000f570cca0bb9dd371f2637ee53d71b91b4446
MadhanRaghul <madhanraghul2002@gmail.com>
2023-12-01 11:04:19 +0530

    'test'

---
+++ datatypes/typesofdata.py
@@ -0,0 +1,13 @@
+int=100
+double=6.9
+string='Hello'
+a=['1','2','3','4']
+b=('1','2','3','4')
+c={'day':'night' , 'love':'death'}
+
+print(type(int))
+print(type(double))
+print(type(string))
+print(type(a))
+print(type(b))
+print(type(c))
