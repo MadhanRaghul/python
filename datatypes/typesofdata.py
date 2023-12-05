db632ee45ba60f023dbe0fa4a094dceb7c05d11d
MadhanRaghul <madhanraghul2002@gmail.com>
2023-12-05 11:01:47 +0530

    sjlkjh

--- datatypes/typesofdata.py
+++ datatypes/typesofdata.py
@@ -1,13 +1,25 @@
 int=100
 double=6.9
 string='Hello'
-a=['1','2','3','4']
-b=('1','2','3','4')
-c={'day':'night' , 'love':'death'}
+list=['1','2','3','4']
+tuple=('1','2','3','4')
+dict={'day':'night' , 'love':'death'}
+set={'one', 'two', 'three'}
+complex=1j
+something=range(7)
+frozen=frozenset({'the', 'frozen', 'set'})
+bool=True
+bytes=k"hi"
 
 print(type(int))
 print(type(double))
 print(type(string))
-print(type(a))
-print(type(b))
-print(type(c))
+print(type(list))
+print(type(tuple))
+print(type(dict))
+print(type(set))
+print(type(complex))
+print(type(something))
+print(type(frozen))
+print(type(bool))
+print(type(bytes))
